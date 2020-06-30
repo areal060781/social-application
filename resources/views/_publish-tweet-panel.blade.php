0ab2cb1658a32126b51fecbf13e9c402c6910f08
Aida Real <areal060781@gmail.com>
2020-06-30 14:25:55 +0000

    Clean sidebar links

--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -2,18 +2,27 @@
     <form action="/tweets" method="post">
         @csrf
 
-        <textarea name="body" placeholder="What's up doc?" class="w-full"></textarea>
+        <textarea name="body"
+                  placeholder="What's up doc?"
+                  class="w-full"
+                  required
+                  autofocus
+        >
+        </textarea>
 
-        <hr class="my-4">
+        <hr class="my-4" >
 
-        <footer class="flex justify-between">
+        <footer class="flex justify-between items-center">
             <img src="{{auth()->user()->avatar}}"
                  alt=""
                  class="rounded-full mr-2"
                  width="50"
                  height="50"
             >
-            <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Tweet-a-roo!</button>
+            <button type="submit"
+                    class="bg-blue-500 hover:bg-blue-600 rounded-lg shadow px-10 text-sm text-white h-10"
+            >
+                Publish</button>
         </footer>
     </form>
 
